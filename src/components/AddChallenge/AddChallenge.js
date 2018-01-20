808f70c808eb72e137bf18c27e36b2cd1d589c00
Toan Nhu Dinh <toanbk21096@gmail.com>
2018-01-20 23:18:37 +0700

    Initial Commit

---
+++ src/components/AddChallenge/AddChallenge.js
@@ -0,0 +1,158 @@
+import React from 'react';
+import Modal from 'react-modal';
+import { Button, InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+var axios = require('axios');
+
+const customStyles = {
+	content: {
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		transform: 'translate(-50%, -50%)'
+	}
+};
+
+class AddChallenge extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			modalIsOpen: false,
+			name: '',
+			description: '',
+      image: '',
+      icon: '',
+			tag: '',
+			status: '',
+			start_time: '',
+			end_time: '',
+			require_user: '',
+			location: '',
+			point: '',
+			creator: ''
+		};
+
+		this.openModal = this.openModal.bind(this);
+		this.afterOpenModal = this.afterOpenModal.bind(this);
+		this.closeModal = this.closeModal.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+		this.onChange = this.onChange.bind(this);
+	}
+
+	openModal() {
+		this.setState({ modalIsOpen: true });
+	}
+
+	afterOpenModal() {
+		// references are now sync'd and can be accessed.
+		this.subtitle.style.color = '#f00';
+	}
+
+	closeModal() {
+		this.setState({ modalIsOpen: false });
+	}
+
+	onChange(evt) {
+		this.setState({ [evt.target.id]: evt.target.value });
+	}
+
+	handleClick() {
+		if (
+			this.state.name.length > 0 &&
+			this.state.description.length > 0 &&
+			this.state.image.length > 0 &&
+			this.state.tag.length > 0 &&
+			this.state.status.length > 0 &&
+			this.state.start_time.length > 0 &&
+			this.state.end_time.length > 0 &&
+			this.state.require_user.length > 0 &&
+			this.state.location.length > 0 &&
+			this.state.point.length > 0 &&
+      this.state.creator.length > 0 &&
+      this.state.icon.length > 0
+    )
+      axios.defaults.headers.common['Authorization'] = 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoibWluaHRoYW5oIiwiaWF0IjoxNTE2NDMwNTk3fQ.1OvbqGr3-K0Ax4gnzjz3t38Y__MmW1to8ICQ33u4XEM';
+      let data = JSON.stringify({
+        name: this.state.name,
+        description: this.state.description,
+        image: this.state.image,
+        icon: this.state.icon,
+        tag: this.state.tag,
+        status: this.state.status,
+        start_time: this.state.start_time,
+        end_time: this.state.end_time,
+        require_user: this.state.require_user,
+        location: this.state.location,
+        point_reward: this.state.point_reward,
+        creator: this.state.creator
+      })
+			axios
+				.post('http://159.89.206.221:4000/challenges', data, {
+					headers: {
+						'Content-Type': 'application/json'
+					}
+        })
+				.then(function(response) {
+          console.log(response);
+					if (response.data.length > 0) {
+            alert('Add New Challenge Success!');
+          }
+				})
+				.catch(function(error) {
+					alert(error);
+				});
+	}
+
+	render() {
+		return (
+			<div>
+				<Button color="primary" onClick={this.openModal}>
+					Add New Challenge
+				</Button>{' '}
+				<Modal
+					isOpen={this.state.modalIsOpen}
+					onAfterOpen={this.afterOpenModal}
+					onRequestClose={this.closeModal}
+					style={customStyles}
+					contentLabel="Add New Challenge"
+				>
+					<h1 ref={(subtitle) => (this.subtitle = subtitle)}>Add New Challenge</h1>
+					<div>
+						<Input id="name" placeholder="Challenge Name" onChange={this.onChange} />
+						<Input id="description" placeholder="Description" onChange={this.onChange} />
+						<Input id="image" placeholder="Image" onChange={this.onChange} />
+            <Input id="icon" placeholder="Icon" onChange={this.onChange} />
+						<Input id="tag" placeholder="Tag" onChange={this.onChange} />
+						<Input id="status" placeholder="Status" onChange={this.onChange} />
+						<Input id="start_time" placeholder="Start Time" onChange={this.onChange} />
+						<Input id="end_time" placeholder="End Time" onChange={this.onChange} />
+						<Input id="require_user" placeholder="Require User" onChange={this.onChange} />
+						<Input id="location" placeholder="Location" onChange={this.onChange} />
+						<Input id="point" placeholder="Point Reward" onChange={this.onChange} />
+						<Input id="creator" placeholder="Creator" onChange={this.onChange} />
+					</div>{' '}
+					<br />
+					<Button color="primary" onClick={this.handleClick}>
+						Add
+					</Button>
+					{'  '}
+					<Button color="danger" onClick={this.openModal}>
+						Close
+					</Button>{' '}
+					<br /> <br />
+					{/* <form>
+						<input /> <br /> <br />
+						<button>tab navigation</button>
+						<button>stays</button>
+						<button>inside</button>
+						<button>the modal</button>
+					</form> */}
+				</Modal>
+			</div>
+		);
+	}
+}
+
+export default AddChallenge;
---
+++ src/views/Pages/Login/Login.js
@@ -0,0 +1,96 @@
+import React, {Component} from 'react';
+import {Container, Row, Col, CardGroup, Card, CardBody, Button, Input, InputGroup, InputGroupAddon} from 'reactstrap';
+var axios = require('axios');
+
+class Login extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+    this.updateUsernameValue = this.updateUsernameValue.bind(this);
+    this.updatePasswordValue = this.updatePasswordValue.bind(this);
+    this.onBtnClick = this.onBtnClick.bind(this);
+  }
+
+  updateUsernameValue(evt){
+    this.setState({
+      username: evt.target.value
+    });
+  }
+
+  updatePasswordValue(evt){
+    this.setState({
+      password: evt.target.value
+    });
+  }
+
+  onBtnClick() {
+    const tokens = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoibWluaHRoYW5oIiwiaWF0IjoxNTE2NDMwNTk3fQ.1OvbqGr3-K0Ax4gnzjz3t38Y__MmW1to8ICQ33u4XEM';
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + tokens;
+    let data = JSON.stringify({
+      phone: this.state.username,
+      password: this.state.password
+  })
+    axios.post('http://159.89.206.221:4000/user/login', data, {
+      headers: {
+          'Content-Type': 'application/json'
+      }
+    })
+    .then(function (response) {
+      if (response.data.token.length > 0) {
+        window.sessionStorage.setItem('token', response.data.token);
+        window.location.href = "http://localhost:8081/#/dashboard";
+      }
+    })
+    .catch(function (error) {
+      alert(error);
+    });
+  }
+
+  render() {
+    return (
+      <div className="app flex-row align-items-center">
+        <Container>
+          <Row className="justify-content-center">
+            <Col md="8">
+              <CardGroup>
+                <Card className="p-4">
+                  <CardBody>
+                    <h1>Login</h1>
+                    <p className="text-muted">Sign In to your account</p>
+                    <InputGroup className="mb-3">
+                      <div className="input-group-prepend">
+                        <span className="input-group-text">
+                          <i className="icon-user"></i>
+                        </span>
+                      </div>
+                      <Input type="text" placeholder="Username" onChange={this.updateUsernameValue}/>
+                    </InputGroup>
+                    <InputGroup className="mb-4">
+                      <div className="input-group-prepend">
+                        <span className="input-group-text">
+                          <i className="icon-lock"></i>
+                        </span>
+                      </div>
+                      <Input type="password" placeholder="Password" onChange={this.updatePasswordValue}/>
+                    </InputGroup>
+                    <Row>
+                      <Col xs="6">
+                        <Button color="primary" className="px-4" onClick={this.onBtnClick}>Login</Button>
+                      </Col>
+                    </Row>
+                  </CardBody>
+                </Card>
+              </CardGroup>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Login;
